2a860ce503832e15c9c8508f0d30454533f0d709
ujjwal rai <ujjwalrai@Ujjwal-Rai-MacBook-Pro.local>
2024-01-22 15:23:18 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,5 @@
+      const parent =React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},React.createElement("h1",{},"i am h1 tag")));
+
+
+      const root=ReactDOM.createRoot(document.getElementById("root"));
+      root.render(parent);
